185081fb32653bd2bc858b6c347889a1195e5efa
Andrei Seabra <andreiamasilio@gmail.com>
2026-05-28 15:14:33 -0300

    add: files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+
+import Home from './pages/Home'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Home() {
+    return (
+        <div className="container">
+            <h1>Pokemons</h1>
+        </div>
+    )
+}
+
+export default Home
